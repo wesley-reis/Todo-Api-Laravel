ba1764f7515b66a586015ad8cea67de8673af331
Wesley <wrrdeveloper@gmail.com>
2021-12-11 13:24:34 -0300

    Implementação do Update e Delete Das Tasks

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\MeController;
 use App\Http\Controllers\Api\TodoController;
+use App\Http\Controllers\TodoTaskController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -26,5 +27,10 @@ Route::prefix('v1')->group(function(){
         Route::put('{todo}', [TodoController::class, 'update']);
         Route::delete('{todo}', [TodoController::class, 'destroy']);
         Route::post('{todo}/tasks', [TodoController::class, 'addTask']);
+    });
+
+    Route::prefix('todo-tasks')->group(function (){
+        Route::put('{todoTask}', [TodoTaskController::class, 'update']);
+        Route::delete('{todoTask}', [TodoTaskController::class, 'destroy']);
     });
 });
